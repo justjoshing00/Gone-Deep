1555e3cbba9ea68022b49a332595715b1c3963b3
josh oji <selmlocke@gmail.com>
2018-08-16 13:37:20 -0400

    changed hardcoded position check to a variable

--- Assets/Scripts/coordinatecheck.cs
+++ Assets/Scripts/coordinatecheck.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class coordinatecheck : MonoBehaviour {
+    Vector3 Position;
+    Vector3 cubePosition1;
 
     
     
@@ -16,10 +18,14 @@ public class coordinatecheck : MonoBehaviour {
         {
 
             transform.position = new Vector3(3, 1, 4);
+            Position = transform.position;// wasted variable, unused 
+            cubePosition1 = new Vector3(4, 1, 4);
+
+
         }
         void Update()
         {
-            if (transform.position( == Vector3(4,1,4))
+            if ( transform.position == cubePosition1)
             {
                 Debug.Log("we did it");
             }
